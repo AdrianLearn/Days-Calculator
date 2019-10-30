a5cec4278d77b99ae9f209e69e4cc51fc19a612b
Adrian Leung <aleung2@lion.lmu.edu>
2019-10-29 19:23:18 -0700

    removal of some comments to not flood the code

--- index.js
+++ index.js
@@ -3,16 +3,13 @@
 * Author: Adrian Leung
 * Date: August 7, 2019
 */
-const date1 = document.getElementById("date1"); // Date1 taken from imput box
-const date2 = document.getElementById("date2"); // Date2 taken from input box
-const resultSpan = document.querySelector('span'); // Reference to span to output days betwen
+const date1 = document.getElementById("date1");
+const date2 = document.getElementById("date2");
+const resultSpan = document.querySelector('span');
 const _MS_PER_DAY = 1000 * 60 * 60 * 24; // Const to hold the millisconds in a day
-date1.addEventListener('input',answer); // EventListeners to run the method if input is detected
-date2.addEventListener('input',answer); // ""
+date1.addEventListener('input',answer);
+date2.addEventListener('input',answer);
 
-/**
-* Function to calculate the distance between the two days and output answer to span
-*/
 function answer(){
   const d1 = new Date(date1.value);
 	const d2 = new Date(date2.value);
